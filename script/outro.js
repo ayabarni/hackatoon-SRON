23f7a5e1274b36fe87c88771d888f8e217081e88
Jean-Carlos2000 <jeandeleon2000@gmail.com>
2026-03-26 05:05:01 +0100

    outro page

---
+++ script/outro.js
@@ -0,0 +1,25 @@
+// bron: https://www.youtube.com/watch?v=pUR5O9UO4Vc
+// bron: https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const video = document.querySelector(".mila-video-bh video");
+
+//     if (!video) return;
+
+//     video.play();
+
+//     requestAnimationFrame(() => {
+//         requestAnimationFrame(() => {
+//             video.classList.add("video-zoom-out");
+//         });
+//     });
+// });
+
+// voeg clas to na 2.5s
+document.addEventListener("DOMContentLoaded", () => {
+    const outroTekst = document.querySelector(".outro-tekst");
+
+    setTimeout(() => {
+        outroTekst.classList.add("is-visible");
+    }, 2500);
+});
